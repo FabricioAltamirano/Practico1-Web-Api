0eacc39570a9c38055362cebf49834834fcd80f8
faltamirano98@hotmail.com <faltamirano98@hotmail.com>
2019-09-06 20:42:58 -0400

    Con Modelo

---
+++ Proyecto 1/Proyecto 1/Models/DbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Proyecto_1.Models
+{
+    public class DBContext
+    {
+    }
+}
---
+++ Proyecto 1/Proyecto 1/Models/Modelo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_1.Models
+{
+    public class Modelo
+    {
+        [Required]
+        public int ModeloID { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public int Salary { get; set; }
+        [Required]
+        public string Birthday { get; set; }
+        public string Recomendation { get; set; }
+
+    }
+ }
